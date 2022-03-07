4c78ca24befc6e6862808bb3ccac27f703682ab6
rma6 <rafaelmarinhoa@gmail.com>
2022-03-07 01:03:58 -0300

    fixed M_z_Tef for array input

--- Hz_out_proj.m
+++ Hz_out_proj.m
@@ -12,14 +12,14 @@ function r = Hz_out_proj(m, p, s, t) %eq 89
                 fun = @(kz) G_Mz_TF0(P.b, n, kz).*(M_z_Tef(n, kz, m, p).*M_z_Tef(-n, -kz, s, t) + M_z_Tef(n, -kz, m, p).*M_z_Tef(-n, kz, s, t) + M_z_Tef(-n, kz, m, p).*M_z_Tef(n, -kz, s, t) + M_z_Tef(-n, -kz, m, p).*M_z_Tef(n, kz, s, t));
             end
 
-%             cZzz2 = 0;
-%             for k=1:P.its.Nkz
-%                 rkzl = (k-1)*P.k0;
-%                 rkzu = k*P.k0;
-%                 cZzz2 = cZzz2 + CGQ1(fun, rkzl, rkzu, 16);
-%             end
+            cZzz2 = 0;
+            for k=1:P.its.Nkz
+                rkzl = (k-1)*P.k0;
+                rkzu = k*P.k0;
+                cZzz2 = cZzz2 + CGQ1(fun, rkzl, rkzu, 16);
+            end
 
-            cZzz2 = CGQ1(fun, 0, P.its.Hz_out_proj.kz, 16);
+%             cZzz2 = CGQ1(fun, 0, P.its.Hz_out_proj.kz, 16);
             acc = acc + cZzz2;
         end
     end
