83798521f189b0549f08bed78f93adf6ee152cfc
rma6 <rafaelmarinhoa@gmail.com>
2024-11-18 21:42:11 -0300

    removed unused functions

--- G_Mz_TFd.m
+++
@@ -1,6 +0,0 @@
-function r = G_Mz_TFd(rho, n, q, P) %eq 33: rho and q might be arrays
-    k_rho = sqrt(P.kd.^2 - (q*pi/P.L).^2);
-    k_rho = cinv(k_rho);
-
-    r = theta_3_rho(n, q, P.kd, rho, P)/(k_rho.*theta_1(n, q, P.kd, P));
-end
