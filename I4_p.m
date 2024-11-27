41546a65a84368fc20bd92cfb78d0c8fd22c82da
rma6 <rafaelmarinhoa@gmail.com>
2024-11-27 00:03:20 -0300

    refactored I4 function

--- I4_p.m
+++ I4_p.m
@@ -1,12 +1,12 @@
 function r = I4_p(kz, p, P) %eq 73: kz might be array
     if p <= (P.Pmax/2)
-	  deltaz = (P.z_1a - P.z1)/(P.Pmax/2);
-	  z_1p = P.z1 + (p-1)*deltaz;
-	  z_2p = P.z1 + p*deltaz;
+	  Lp = (P.z_1a - P.z1)/(P.Pmax/2);
+	  z_1p = P.z1 + (p-1)*Lp;
+	  z_2p = P.z1 + p*Lp;
 	else
-	  deltaz = (P.z2 - P.z_2a)/(P.Pmax/2);
-	  z_1p = P.z_2a + (p-P.Pmax/2-1)*deltaz;
-	  z_2p = P.z_2a + (p-P.Pmax/2)*deltaz;
+	  Lp = (P.z2 - P.z_2a)/(P.Pmax/2);
+	  z_1p = P.z_2a + (p-P.Pmax/2-1)*Lp;
+	  z_2p = P.z_2a + (p-P.Pmax/2)*Lp;
     end
 
     r = (exp(1j*kz*z_2p)-exp(1j.*kz.*z_1p))./(1j*kz);
